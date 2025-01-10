3817188061ce4f793b1fbb4896080994e97bd24f
samuel <samuel@iesjuanbosco.es>
2025-01-10 18:27:26 +0100

    Commit

---
+++ src/main/java/com/dwes/empleadosapi/entities/Proyecto.java
@@ -0,0 +1,9 @@
+package com.dwes.empleadosapi.entities;
+/**
+ * Crear una Entidad llamada Proyecto con los datos "nombre", "presupuesto", "descripción" y "fechaInicio".
+ * Implementar una API REST para poder obtener todas, obtener una, actualizar, crear y borrar.
+ */
+
+public class Proyecto {
+
+}
